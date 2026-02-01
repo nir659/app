5b0060924b42ea193eba3ac1069baacef5b1f8cd
nir659 <nir659@users.noreply.github.com>
2026-02-02 04:59:22 +1000

    Update audio source and volume in audio.js

--- html/js/audio.js
+++ html/js/audio.js
@@ -1,6 +1,6 @@
 document.getElementById("overlay").addEventListener("click", function() {
     document.getElementById("overlay").style.display = "none";
-    var audio = new Audio('/assets/eh.mp3');
+    var audio = new Audio('/assets/AM.mp3');
     audio.volume = 0.2;
     audio.play();
 });
